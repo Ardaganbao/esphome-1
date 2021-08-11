13db9d91619629c9d7f9cd53dde1e93a1b0ba201
Ardaganbao <35694295+Ardaganbao@users.noreply.github.com>
2021-08-11 08:51:24 +0200

    fix4 try 4

--- esphome/components/modbus_slave/modbus_slave.cpp
+++ esphome/components/modbus_slave/modbus_slave.cpp
@@ -5,14 +5,11 @@
 namespace esphome {
 namespace modbus_slave {
 
+uint8_t input_pins[] = {2, 3, 4};  // Add the pins you want to read as a Discrete input.
 
-uint8_t input_pins[] = {2, 3, 4};     // Add the pins you want to read as a Discrete input.
- 
 // You shouldn't have to change anything below this to get this example to work
 
-uint8_t input_pins_size = sizeof(input_pins) / sizeof(input_pins[0]);    // Get the size of the input_pins array
- 
-
+uint8_t input_pins_size = sizeof(input_pins) / sizeof(input_pins[0]);  // Get the size of the input_pins array
 
 static const char *const TAG = "modbus_slave";
 #define readCRC(arr, length) word(arr[(length - MODBUS_CRC_LENGTH) + 1], arr[length - MODBUS_CRC_LENGTH])
@@ -24,18 +21,13 @@ void ModbusSlaveESP::setup() {
 
   uint32_t baudrate = this->parent_->get_baud_rate();
 
+  // Set the defined input pins to input mode.
+  for (int i = 0; i < input_pins_size; i++) {
+    pinMode(input_pins[i], INPUT);
+  }
 
-    // Set the defined input pins to input mode.
-    for (int i = 0; i < input_pins_size; i++)
-    {
-        pinMode(input_pins[i], INPUT);
-    }
- 
- 
-
-    // Register functions to call when a certain function code is received. 
-  //  this->cbVector[CB_READ_DISCRETE_INPUTS] = readDigitalIn; 
-
+  // Register functions to call when a certain function code is received.
+  //  this->cbVector[CB_READ_DISCRETE_INPUTS] = readDigitalIn;
 }
 // read any new data from uart
 void ModbusSlaveESP::read_uart() {
@@ -56,13 +48,10 @@ void ModbusSlaveESP::read_uart() {
   }
 }
 
-void ModbusSlaveESP::loop() {
-// read_uart();
- 
-}
+void ModbusSlaveESP::loop() { read_uart(); }
 bool ModbusSlaveESP::parse_modbus_byte_(uint8_t byte) {
- ESP_LOGW(TAG, "arda: parse_modbus_byte_  0x%X", byte);
-return false;
+  ESP_LOGW(TAG, "arda: parse_modbus_byte_  0x%X", byte);
+  return false;
 }
 void ModbusSlaveESP::dump_config() {
   ESP_LOGCONFIG(TAG, "ModbusSlaveESP:");
@@ -74,15 +63,9 @@ float ModbusSlaveESP::get_setup_priority() const {
   return setup_priority::BUS - 1.0f;
 }
 
- 
-
-// Handle the function code Read Input Status (FC=02) and write back the values from the digital input pins (discreet input).
-uint8_t  readDigitalIn(uint8_t fc, uint16_t address, uint16_t length)
-{
-   
-    return STATUS_OK;
-}
-  
+// Handle the function code Read Input Status (FC=02) and write back the values from the digital input pins (discreet
+// input).
+uint8_t readDigitalIn(uint8_t fc, uint16_t address, uint16_t length) { return STATUS_OK; }
 
 }  // namespace modbus_slave
 }  // namespace esphome
